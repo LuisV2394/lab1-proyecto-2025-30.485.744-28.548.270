64d953afcc701bada1978f73c10828ba324026d4
Willian Molina <1001.28548270.ucla@gmail.com>
2025-11-23 12:40:12 -0400

    modifies the following files: agenda.py, appointment.py when creating or updating appointments to ensure proper foreign key relationships.

--- app/models/agenda.py
+++ app/models/agenda.py
@@ -6,9 +6,12 @@ db = SQLAlchemy() # Asumiendo que inicializas db en app.py
 class Block(db.Model):
     __tablename__ = 'blocks'
     id = db.Column(db.Integer, primary_key=True)
-    professional_id = db.Column(db.Integer, nullable=False)
-    unit_id = db.Column(db.Integer, nullable=False)
+    professional_id = db.Column(db.Integer, db.ForeignKey("professionals.id") ,  nullable=False)
+    unit_id = db.Column(db.Integer, db.ForeignKey("units.id"), nullable=False)
     start = db.Column(db.DateTime, nullable=False)
     end = db.Column(db.DateTime, nullable=False)
     ability = db.Column(db.Integer, default=1) # Capacidad (ej. 2 pacientes simultáneos)
     state = db.Column(db.String(20), default='open') # open, close, reserved
+
+    professional = db.relationship("Professional", backref=db.backref("blocks", lazy=True))
+    unit = db.relationship("Unit", backref=db.backref("blocks", lazy=True))
--- app/models/appointment.py
+++ app/models/appointment.py
@@ -4,9 +4,9 @@ from datetime import datetime
 class Appointment(db.Model):
     __tablename__ = 'appointments'
     id = db.Column(db.Integer, primary_key=True)
-    person_id = db.Column(db.Integer, nullable=False)
-    professional_id = db.Column(db.Integer, nullable=False)
-    unit_id = db.Column(db.Integer, nullable=False)
+    person_id = db.Column(db.Integer,db.ForeignKey("people.id") , nullable=False)
+    professional_id = db.Column(db.Integer, db.ForeignKey("professionals.id") , nullable=False)
+    unit_id = db.Column(db.Integer, db.ForeignKey("units.id") , nullable=False)
     start = db.Column(db.DateTime, nullable=False)
     end = db.Column(db.DateTime, nullable=False)
     reason = db.Column(db.String(255))
@@ -16,6 +16,9 @@ class Appointment(db.Model):
     
     # Relación para validaciones
     block_id = db.Column(db.Integer, db.ForeignKey('blocks.id'), nullable=True)
+    person = db.relationship("Person", backref=db.backref("appointments", lazy=True))
+    professional = db.relationship("Professional", backref=db.backref("appointments", lazy=True))
+    unit = db.relationship("Unit", backref=db.backref("appointments", lazy=True))
 
 class AppointmentHistory(db.Model):
     __tablename__ = 'appointment_history'
