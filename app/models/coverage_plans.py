bf9b50077e4a4177c6189db23b1a5508af6bfcd3
Willian Molina <1001.28548270.ucla@gmail.com>
2026-01-05 11:09:10 -0400

    update insurance tablename and coverage plan tablename

--- app/models/coverage_plans.py
+++ app/models/coverage_plans.py
@@ -7,6 +7,7 @@ class CoveragePlan(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     
     insurer_id = db.Column(db.Integer, db.ForeignKey('insurance.id'), nullable=False)
+    
     name = db.Column(db.String(100), nullable=False)
     general_conditions = db.Column(db.Text) 
     
--- app/models/insurance.py
+++ app/models/insurance.py
@@ -2,7 +2,7 @@ from app import db
 from datetime import datetime
 
 class InsuranceCompany(db.Model):
-    __tablename__ = 'insurance'
+    __tablename__ = 'insurers'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(150), nullable=False)
