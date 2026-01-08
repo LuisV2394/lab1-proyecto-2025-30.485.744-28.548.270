fbcbaeb1e1aacc9523a469b1d656ec9994ff6ca1
Luis Daza <ldaza2394@gmail.com>
2026-01-07 20:22:30 -0400

    chore(migrations): register new models for Flask-Migrate detection

--- manage.py
+++ manage.py
@@ -14,9 +14,18 @@ from app.models.user import User
 from app.models.appointment import Appointment
 from app.models.consents import Consent
 from app.models.diagnoses import Diagnosis
-from app.models.note import ClinicalNote    
 from app.models.role import Role
 from app.models.user_role import UserRole
+from app.models.afiliation import Affiliation  
+from app.models.authorization import Authorization
+from app.models.coverage_plans import CoveragePlan
+from app.models.credit_debit_note import CreditDebitNote
+from app.models.orders import Order
+from app.models.order_details import OrderDetail
+from app.models.prescription import Prescription, PrescriptionItem
+from app.models.payer import Payer
+from app.models.payments import Payment
+from app.models.result import Result
 
 app = create_app()
 migrate = Migrate(app, db)
